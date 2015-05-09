d3c23301e35d0d7a22e0ddb7c0dee0ba477d9c27
Alexander <abdolak@fullsail.edu>
2015-05-09 09:22:18 -0400

    Day 2 code
    starting to learn vars and how to manipulate them

---
+++ js/Script.js
@@ -0,0 +1,27 @@
+/*
+--------------------------
+    Alexander Dolak
+    SDI 1505
+    Day Two
+    Fun with Data Types
+--------------------------
+ */
+// comment
+
+// this is where I am starting the variables
+var myHomeAddress   =   "123 Elm Street";
+var milesToStore    =   8.5;
+//var construction    =   false;
+var myFavoriteStore =   "Publix Supermarket";
+
+// adding confirm
+//confirm();
+var construction = confirm("is there construction?" + "Click OK for yes or clich Cancle for no.")
+
+
+
+// these are outputs
+console.log("I live at " + myHomeAddress + ".");
+console.log("I shop at " + myFavoriteStore + ".");
+console.log("It's only " + milesToStore + " to the store.");
+console.log("It's " + construction + " that there is construction onroute.");
